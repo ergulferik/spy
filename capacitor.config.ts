bf80844e7e5beeae4e0d3cd5a3868c7de3f123ea
Ergül FERİK <140970403+ergulferik@users.noreply.github.com>
2025-01-24 22:23:41 +0300

    config update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'spy',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    // GitHub Pages'de gerekli olan base url yapılandırması
+    url: '/spy/', // Repository adını buraya yaz (eğer repository adı farklıysa güncelle)
+    cleartext: true // Geliştirme sırasında sorun yaşamamak için
+  }
 };
 
 export default config;
